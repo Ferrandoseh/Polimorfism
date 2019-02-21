c45244e69fb71d5345e344133519d090a4f5c4bb
Ferran Ramírez <ferranramireznavajon@gmail.com>
2019-02-21 12:45:08 +0100

    First commit

--- Polimorfismo/Program.cs
+++ Polimorfismo/Program.cs
@@ -8,8 +8,44 @@ namespace Polimorfismo
 {
     class Program
     {
+        abstract class Accomodation
+        {
+            public virtual void VisitIt()
+            {
+                Console.WriteLine("I'm visiting a random accomodation before I buy it");
+            }
+            public abstract void GoToSecondFloor();
+        }
+        class Flat : Accomodation
+        {
+            public new void VisitIt()
+            {
+                Console.WriteLine("I'm visiting my friend's flat");
+            }
+            public override void GoToSecondFloor()
+            {
+                Console.WriteLine("I'm taking the lift");
+            }
+        }
+        class House : Accomodation
+        {
+            public override void GoToSecondFloor()
+            {
+                Console.WriteLine("I'm going upstairs");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Accomodation flat = new Flat();
+            Accomodation house = new House();
+            flat.GoToSecondFloor();
+            flat.VisitIt();
+            house.GoToSecondFloor();
+            house.VisitIt();
+
+            Console.Read();
+
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# Polimorfism
+###Difference between the *new* and *override* annotations.
+
+After the declarison of a method as *virtual*
+and then overriding it in its child with the *override* keyword 
+against simply using the *new* keyboard when declaring it it's 
+basically *override* is literally rewriting the method inherited
+from the main class since *new* is just creating a new method with
+the same name as in the main class although it's not referenced to
+ it, because it's just a new member with the same name and clauses.
